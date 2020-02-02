8bd7eeb4127789ee195f172feba2289484a18ae7
Y4er <45418382@qq.com>
2020-02-03 00:28:53 +0800

    auto download github image of post.

---
+++ build/img.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests
+import re
+import os,sys
+from datetime import datetime
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+}
+'''
+proxies = {
+    'http': 'socks5://127.0.0.1:1080',
+    'https': 'socks5://127.0.0.1:1080',
+}
+'''
+def now():
+    return str(datetime.now().strftime("%Y%m%d%H") + str(datetime.now().microsecond)[-4:])
+
+def mkdir(path):
+    folder = os.path.exists(path)
+    if not folder:
+        print("------------------- mkdir -------------------")
+        os.makedirs(path)
+
+if __name__ == '__main__':
+    print("------------------- start -------------------")
+    githubusercontent = r'(!.*(https://.*github.*.png).*\))'
+    postdir = './content/post/'
+    try:
+        for post in os.listdir(postdir):
+            if post[-2:] == 'md':
+                f = open(postdir+post, 'r', encoding='utf8')
+                content = f.read()
+                f.close()
+                if 'user-images.githubusercontent.com' in content:
+                    imgs = re.findall(githubusercontent, content)
+                    for markdown, img in imgs:
+                        # 保存图片
+                        # imgcontent = requests.get(img, headers=headers, proxies=proxies).content
+                        imgcontent = requests.get(img, headers=headers).content
+                        mkdir("static/img/uploads/")
+                        filename = 'img/uploads/' + now() + '.png'
+                        with open('static/'+filename, 'wb+') as mark:
+                            mark.write(imgcontent)
+                            print("[!][{}] \t {} \t {}".format(post,img, filename))
+                        # 替换文章markdown链接
+                        markdown_str = markdown
+                        markdown = markdown.replace(img, sys.argv[1] + filename)
+                        content = content.replace(markdown_str, markdown)
+                        with open(postdir+post, 'w', encoding='utf8') as file:
+                            file.write(content)
+                else:
+                    print("[*][{}] \t not found.".format(post))
+        print("------------------- over! -------------------")
+    except Exception as e:
+        print("------------------- error -------------------")
+        print("[x]error: {}".format(e))
+        print("------------------- error -------------------")
